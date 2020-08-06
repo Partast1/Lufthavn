afc86ab57c3c0a507e10e29148b6f63fd3ab1a03
Partast1 <43130360+Partast1@users.noreply.github.com>
2020-08-06 18:47:35 +0200

    a

---
+++ AirContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lufthavn
+{
+    class AirContext : DbContext
+    {
+        public AirContext() 
+        {
+        
+
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            throw new UnintentionalCodeFirstException();
+        }
+        public virtual DbSet<Airplane> irplanes { get; set; }
+        public virtual DbSet<Airline> airlines { get; set; }
+
+        public virtual DbSet<Airport> airports { get; set; }
+
+        public virtual DbSet<Rute> rutes { get; set; }
+
+        public virtual DbSet<Airplane> Courses { get; set; }
+
+    }
+}
